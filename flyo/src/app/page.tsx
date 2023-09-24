d173c8e0aca945352ddfd28b78b4cbc205a92fb1
Nicolas Korsunski <84050410+letmegro@users.noreply.github.com>
2023-09-23 22:11:49 -0400

    configured styles and fonts for the project

--- flyo/src/app/page.tsx
+++ flyo/src/app/page.tsx
@@ -1,9 +1,29 @@
 import Image from 'next/image'
+import { Raleway } from 'next/font/google';
+import { Open_Sans } from 'next/font/google';
+
+const raleway = Raleway({
+  subsets: ['latin'],
+  weight: ['400', '400', '700'],
+  style: ['normal', 'italic', 'normal'],
+  variable: '--raleway'
+});
+ 
+const openSans = Open_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  style: ['normal', 'normal'],
+  variable: '--open_sans'
+});
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+  <>
+    <main style={raleway.style}>
+      <div className='bg-white dark:bg-red-500'>
+        <h1 className={`text-3xl`}>West</h1>
+      </div>
+    </main>
+  </>
   )
 }
--- flyo/tailwind.config.ts
+++ flyo/tailwind.config.ts
@@ -6,13 +6,29 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        darkBlue: 'hsl(217, 28%, 15%)',
+        darkBlue1: 'hsl(218, 28%, 13%)',
+        darkBlue2: 'hsl(216, 53%, 9%)',
+        darkBlue3: 'hsl(219, 30%, 18%)',
+        accentCyan: 'hsl(176, 68%, 64%)',
+        accentBlue: 'hsl(198, 60%, 50%)',
+        lightRed: 'hsl(0, 100%, 63%)',
       },
+      backgroundImage: (theme) => ({
+        'logo-dark-mode': "url('public/images/logo-dark-mode.svg')",
+        'logo-light-mode': "url('public/images/logo-light-mode.svg')",
+        'curvy-dark-mode': "url('public/images/bg-curvy-dark-mode.svg')",
+        'curvy-light-mode': "url('public/images/bg-curvy-light-mode.svg')",
+      }),
+    },
+  },
+  variants: {
+    extend: {
+      backgroundImage: ['dark'],
     },
   },
   plugins: [],
